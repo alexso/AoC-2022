ffe082a5f4ceffe79726d833e29b6cc42652dc91
Alex So <alex.so@openpos.tech>
2022-12-06 06:28:26 +0100

    Day 6

---
+++ src/main/kotlin/Day6.kt
@@ -0,0 +1,35 @@
+import java.io.File
+
+fun main(args: Array<String>) {
+    solutionPart1()
+    solutionPart2()
+}
+
+private fun solutionPart1() {
+    val inputData = File("inputData/Input6.txt").readText()
+    println("Part 1 - needed length is: ${inputData.charsNeededToFirstUniqueSequence(4)}")
+}
+private fun solutionPart2() {
+    val inputData = File("inputData/Input6.txt").readText()
+    println("Part 2 - needed length is: ${inputData.charsNeededToFirstUniqueSequence(14)}")
+}
+
+fun String.charsNeededToFirstUniqueSequence(uniqueLength: Int): Int {
+    var newString: String = ""
+    this.forEach {
+        newString += it
+        val reversedString = newString.reversed().take(uniqueLength)
+        if (reversedString.count() >= uniqueLength) {
+            if (reversedString.allUnique()) {
+                return newString.count()
+            }
+        }
+    }
+    return newString.count()
+}
+
+fun String.allUnique(): Boolean {
+    val set = mutableSetOf<Char>()
+    set.addAll(this.asSequence())
+    return set.size == this.length
+}
